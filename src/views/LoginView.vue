753657ec06bdc43c365976aaea3b97f2fef8dcb5
SeePeaJay <cpj99@proton.me>
2023-10-30 05:37:50 -0700

    Add basic validation to login and sign up
    Now displays warnings if credentials are empty

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,24 +1,24 @@
 <script setup lang="ts">
-import { ref, computed } from "vue";
-// import { useVuelidate } from '@vuelidate/core'
-// import { required } from '@vuelidate/validators'
+import { useForm } from "vee-validate";
 
-const username = ref("");
-const password = ref("");
-// const rules = computed(() => (
-//   {
-//     username: { required },
-//     password: { required },
-//   }
-// ));
-// const $v = useVuelidate(rules, { username, password });
+function required(value: string) {
+  return value ? true : "This field is required!";
+}
 
-const submitForm = () => {
-  // $v.value.$touch()
-  // if (!$v.value.$invalid) {
-  //   // Handle login
-  // }
-};
+const { defineInputBinds, handleSubmit, errors } = useForm({
+  validationSchema: {
+    username: required,
+    password: required,
+  },
+});
+
+const username = defineInputBinds("username");
+const password = defineInputBinds("password");
+
+const onSubmit = handleSubmit((values) => {
+  // Submit to API
+  console.log(values);
+});
 </script>
 
 <template>
@@ -28,19 +28,35 @@ const submitForm = () => {
         <h1>Login</h1>
         <span>Welcome back</span>
       </div>
-      <div class="form-control w-full max-w-xs">
+      <form class="form-control w-full max-w-xs" @submit="onSubmit">
         <label class="label">
           <span class="label-text">Username</span>
         </label>
-        <input type="text" class="input input-bordered w-full max-w-xs" />
+        <input
+          type="text"
+          v-bind="username"
+          class="input input-bordered w-full max-w-xs"
+          :class="{ 'input-error': errors.username }"
+        />
+        <label class="label">
+          <span class="label-text-alt text-red-500">{{ errors.username }}</span>
+        </label>
         <label class="label">
           <span class="label-text">Password</span>
         </label>
-        <input type="password" class="input input-bordered w-full max-w-xs" />
+        <input
+          type="password"
+          v-bind="password"
+          class="input input-bordered w-full max-w-xs"
+          :class="{ 'input-error': errors.password }"
+        />
+        <label class="label">
+          <span class="label-text-alt text-red-500">{{ errors.password }}</span>
+        </label>
         <label class="label"></label>
         <button class="btn">Submit</button>
         <span>Don't have an account? <RouterLink class="pressable link" to="/signup"> Sign Up</RouterLink></span>
-      </div>
+      </form>
     </div>
   </div>
 </template>
--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,24 +1,26 @@
 <script setup lang="ts">
-import { ref, computed } from "vue";
-// import { useVuelidate } from '@vuelidate/core'
-// import { required } from '@vuelidate/validators'
+import { useForm } from "vee-validate";
 
-const username = ref("");
-const password = ref("");
-// const rules = computed(() => (
-//   {
-//     username: { required },
-//     password: { required },
-//   }
-// ));
-// const $v = useVuelidate(rules, { username, password });
+function required(value: string) {
+  return value ? true : "This field is required!";
+}
 
-const submitForm = () => {
-  // $v.value.$touch()
-  // if (!$v.value.$invalid) {
-  //   // Handle login
-  // }
-};
+const { defineInputBinds, handleSubmit, errors } = useForm({
+  validationSchema: {
+    username: required,
+    password: required,
+    confirmPassword: required,
+  },
+});
+
+const username = defineInputBinds("username");
+const password = defineInputBinds("password");
+const confirmPassword = defineInputBinds("confirmPassword");
+
+const onSubmit = handleSubmit((values) => {
+  // Submit to API
+  console.log(values);
+});
 </script>
 
 <template>
@@ -28,23 +30,47 @@ const submitForm = () => {
         <h1>Sign Up</h1>
         <span>Just a few things to get started</span>
       </div>
-      <div class="form-control w-full max-w-xs">
+      <form class="form-control w-full max-w-xs" @submit="onSubmit">
         <label class="label">
           <span class="label-text">Username</span>
         </label>
-        <input type="text" class="input input-bordered w-full max-w-xs" />
+        <input
+          type="text"
+          v-bind="username"
+          class="input input-bordered w-full max-w-xs"
+          :class="{ 'input-error': errors.username }"
+        />
+        <label class="label">
+          <span class="label-text-alt text-red-500">{{ errors.username }}</span>
+        </label>
         <label class="label">
           <span class="label-text">Password</span>
         </label>
-        <input type="password" class="input input-bordered w-full max-w-xs" />
+        <input
+          type="password"
+          v-bind="password"
+          class="input input-bordered w-full max-w-xs"
+          :class="{ 'input-error': errors.password }"
+        />
+        <label class="label">
+          <span class="label-text-alt text-red-500">{{ errors.password }}</span>
+        </label>
         <label class="label">
           <span class="label-text">Confirm Password</span>
         </label>
-        <input type="password" class="input input-bordered w-full max-w-xs" />
+        <input
+          type="password"
+          v-bind="confirmPassword"
+          class="input input-bordered w-full max-w-xs"
+          :class="{ 'input-error': errors.confirmPassword }"
+        />
+        <label class="label">
+          <span class="label-text-alt text-red-500">{{ errors.confirmPassword }}</span>
+        </label>
         <label class="label"></label>
         <button class="btn">Submit</button>
         <span>Already a user? <RouterLink class="pressable link" to="/login"> Login</RouterLink></span>
-      </div>
+      </form>
     </div>
   </div>
 </template>

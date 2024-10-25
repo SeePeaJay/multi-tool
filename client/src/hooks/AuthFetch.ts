e7276f99f7a0063fcec79d3f6b3220fdb3ea8579
SeePeaJay <cpj99@proton.me>
2024-10-24 20:00:45 -0700

    create custom fetch for App and Notes pages to retrieve backend data

---
+++ client/src/hooks/AuthFetch.ts
@@ -0,0 +1,38 @@
+/* A fetch wrapper that sets auth state to false and redirects if a 401 or 500 response is encountered. */
+
+import { useAuth } from "../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+export const useAuthFetch = () => {
+  const { setIsAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  const authFetch = async (url: string, options = {}) => {
+    try {
+      const response = await fetch(url, options);
+
+      if (!response.ok) {
+        if (response.status === 401 || response.status === 500) {
+          setIsAuthenticated(false);
+          navigate("/");
+          return; // exit early to avoid further processing
+        } else {
+          throw new Error("Network response was not ok");
+        }
+      }
+
+      // check content-type header to determine how to process response
+      const contentType = response.headers.get("content-type");
+      if (contentType && contentType.includes("application/json")) {
+        return await response.json();
+      } else {
+        return await response.text();
+      }
+    } catch (error) {
+      console.error("Fetch error:", error);
+      throw error;
+    }
+  };
+
+  return authFetch;
+};
--- client/src/routes/Notes.tsx
+++ client/src/routes/Notes.tsx
@@ -1,4 +1,30 @@
+import { useEffect } from "react";
+import { useAuthFetch } from "../hooks/AuthFetch";
+
 function Notes() {
+  const authFetch = useAuthFetch();
+
+  const fetchNotes = async () => {
+    try {
+      const response = await authFetch(
+        `http://localhost:3000/api/notes`,
+        { credentials: "include" }, // include cookies with request; required for cookie session to function
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch notes");
+      }
+
+      console.log(response);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
   return (
     <div className="mx-auto p-8 w-[90vw] lg:w-[50vw]">
       <h1>All Notes</h1>

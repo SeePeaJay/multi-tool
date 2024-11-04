a3756187f9a33065227bb78aed139e770a6d44fc
SeePeaJay <cpj99@proton.me>
2024-11-03 19:15:33 -0800

    setup server error notification

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,15 +1,29 @@
 import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import SessionChecker from "../components/SessionChecker";
 import Navbar from "../components/Navbar";
 import Root from "../routes/Root";
 import Starred from "../routes/Starred";
 import Notes from "../routes/Notes";
 import ProtectedRoute from "../routes/ProtectedRoute";
+import "react-toastify/dist/ReactToastify.min.css";
 
 function App() {
   return (
     <>
       <SessionChecker />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
       <Navbar />
       <Routes>
         <Route path="/" element={<Root />} />
--- client/src/hooks/AuthFetch.ts
+++ client/src/hooks/AuthFetch.ts
@@ -4,9 +4,9 @@
   This is primarily designed to block unauthorized users from accessing GET endpoints.
 */
 
-
-import { useAuth } from "../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useAuth } from "../contexts/AuthContext";
 
 export const useAuthFetch = () => {
   const { setIsAuthenticated } = useAuth();
@@ -16,14 +16,22 @@ export const useAuthFetch = () => {
     try {
       const response = await fetch(url, options);
 
+      // for the time being, immediately exit upon any server error code
       if (!response.ok) {
-        if (response.status === 401 || response.status === 500) {
-          setIsAuthenticated(false);
-          navigate("/");
-          return; // exit early to avoid further processing
-        } else {
-          throw new Error("Network response was not ok");
-        }
+        setIsAuthenticated(false);
+        navigate("/");
+        toast.error(`Network response was not ok! Error code: ${response.status}`, {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+
+        return; // exit early to avoid further processing
       }
 
       // check content-type header to determine how to process response

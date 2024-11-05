f55fb181c1540c3c871d8fc6b1c06eccda64387f
SeePeaJay <cpj99@proton.me>
2024-11-04 21:09:03 -0800

    adjust loading screen position

--- client/src/components/LoadingScreen.tsx
+++ client/src/components/LoadingScreen.tsx
@@ -1,6 +1,6 @@
 function LoadingScreen() {
   return (
-    <div className="flex h-[100vh] w-[100vw] items-center justify-center">
+    <div className="fixed top-0 z-50 flex h-[100vh] w-[100vw] items-center justify-center">
       <h1>Loading ...</h1>
     </div>
   );

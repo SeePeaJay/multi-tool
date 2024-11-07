4a692b213f7ab70cbc6414f0b0fbf77c8b289278
SeePeaJay <cpj99@proton.me>
2024-11-07 01:51:09 -0800

    adjust spacing between blocks

--- client/src/components/SkeletonEditor.tsx
+++ client/src/components/SkeletonEditor.tsx
@@ -1,6 +1,6 @@
 const SkeletonEditor = () => {
   return (
-    <div className="mt-1 space-y-2">
+    <div className="mt-5 space-y-2"> {/* more margin than heading to align with actual content */}
       <div className="h-4 bg-gray-300 rounded w-full animate-pulse"></div>
       <div className="h-4 bg-gray-300 rounded w-full animate-pulse"></div>
       <div className="h-4 bg-gray-300 rounded w-full animate-pulse"></div>

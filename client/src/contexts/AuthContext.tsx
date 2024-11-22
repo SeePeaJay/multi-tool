4bfabca029ef6f4486ace34d0c28ad7fd589a43c
SeePeaJay <cpj99@proton.me>
2024-11-22 01:09:06 -0800

    reuse localstorage for auth state
    * It appears the async nature of dexie/indexeddb makes tracking auth state unpredictable across refresh

--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -20,25 +20,19 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+   // initialize state with localStorage to persist auth status across refreshes
+   const [isAuthenticated, setIsAuthenticated] = useState(() => {
+    const storedAuthState = localStorage.getItem("isAuthenticated");
+    return storedAuthState ? JSON.parse(storedAuthState) : false;
+  });
 
-  // initialize state with dexie to persist auth status across refreshes
-  useEffect(() => {
-    const loadAuthState = async () => {
-      const storedAuthState = await db.table("auth").get("isAuthenticated");
-      setIsAuthenticated(storedAuthState?.value || false);
-    };
-
-    loadAuthState();
-  }, []);
-
-  // keep dexie up to date
+  // keep localstorage/dexie up to date
   useEffect(() => {
     const saveAuthState = async () => {
       if (isAuthenticated) {
-        await db.auth.put({ key: "isAuthenticated", value: isAuthenticated });
+        localStorage.setItem("isAuthenticated", JSON.stringify(isAuthenticated));
       } else {
-        await db.auth.put({ key: "isAuthenticated", value: false });
+        localStorage.clear();
         await db.notes.clear();
       }
     };
--- client/src/db.ts
+++ client/src/db.ts
@@ -5,13 +5,11 @@
 import Dexie, { type EntityTable } from "dexie";
 
 const db = new Dexie("AppDatabase") as Dexie & {
-  auth: EntityTable<{ key: string; value: boolean }>;
   notes: EntityTable<{ key: string; content: string }>;
 };
 
 // Schema declaration:
 db.version(1).stores({
-  auth: "key",
   notes: "key",
 });
 

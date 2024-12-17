c7e2b50032909b3f1d778062fa0057d1492b5241
SeePeaJay <cpj99@proton.me>
2024-12-17 02:16:29 -0800

    add table and search bar to notes page

--- client/src/routes/Notes.tsx
+++ client/src/routes/Notes.tsx
@@ -1,25 +1,47 @@
-import { useEffect } from "react";
-import { useAuthFetch } from "../hooks/AuthFetch";
+import { useLiveQuery } from "dexie-react-hooks";
+import { useState } from "react";
+import { HashLink } from "react-router-hash-link";
+import { db } from "../db";
 
 function Notes() {
-  const authFetch = useAuthFetch();
+  const notes = useLiveQuery(() => db.notes.toArray(), []);
 
-  const fetchNotes = async () => {
-    const response = await authFetch(
-      `/api/notes`,
-      { credentials: "include" }, // include cookies with request; required for cookie session to function
-    );
-
-    console.log(response);
-  };
-
-  useEffect(() => {
-    fetchNotes();
-  }, []);
+  const [searchTerm, setSearchTerm] = useState("");
 
   return (
-    <div className="mx-auto p-8 w-[90vw] lg:w-[50vw]">
+    <div className="mx-auto w-[90vw] p-8 lg:w-[50vw]">
       <h1>All Notes</h1>
+
+      <input
+        type="text"
+        placeholder="Search notes..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="input input-sm input-bordered mb-2 w-full"
+      />
+
+      <table className="table table-sm">
+        <thead>
+          <tr>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          {notes &&
+            notes.map((note) => (
+              <tr key={note.id}>
+                <td>
+                  <HashLink
+                    className="text-blue-600 hover:underline"
+                    to={`/app/notes/${note.id}`}
+                  >
+                    {note.title}
+                  </HashLink>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 }

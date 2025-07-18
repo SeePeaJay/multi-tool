0fc3db5bfabc37ac67314a4af91211bcf012249e
SeePeaJay <cpj99@proton.me>
2025-07-18 01:09:42 -0700

    modify tests for offline-first

--- client/src/components/TitleEditor.cy.tsx
+++ client/src/components/TitleEditor.cy.tsx
@@ -1,8 +1,10 @@
 import { MemoryRouter } from "react-router-dom";
 import { getDefaultYdocUpdate } from "shared";
+import * as Y from "yjs";
 import { db } from "../db";
 import { AuthProvider } from "../contexts/AuthContext";
 import { LoadingProvider } from "../contexts/LoadingContext";
+import { SessionProvider } from "../contexts/SessionContext";
 import { StatelessMessengerProvider } from "../contexts/StatelessMessengerContext";
 import TitleEditor from "./TitleEditor";
 
@@ -21,9 +23,11 @@ it("renames correctly", () => {
     <MemoryRouter initialEntries={["/app/notes/bbbbbb"]}>
       <AuthProvider>
         <LoadingProvider>
-          <StatelessMessengerProvider>
-            <TitleEditor noteId="bbbbbb" />
-          </StatelessMessengerProvider>
+          <SessionProvider>
+            <StatelessMessengerProvider>
+              <TitleEditor noteId="bbbbbb" />
+            </StatelessMessengerProvider>
+          </SessionProvider>
         </LoadingProvider>
       </AuthProvider>
     </MemoryRouter>,
@@ -35,5 +39,18 @@ it("renames correctly", () => {
   cy.wait(1000); // wait for db update; below is not designed to rerun when assertion fails
   cy.then(() => db.notes.get({ title: "testing" })).should((note) => {
     expect(note?.title).to.contain("testing");
+  });
+  cy.then(() => db.user.get(0)).should((user) => {
+    expect(user).to.not.equal(undefined);
+
+    const metadataYdocArray = user!.metadataYdocArray;
+    const ydoc = new Y.Doc();
+    Y.applyUpdate(ydoc, new Uint8Array(metadataYdocArray));
+    const noteMetadata = ydoc.getMap("noteMetadata");
+    const noteIsRenamedInMetadata =
+      Array.from(noteMetadata.values()).includes("testing") &&
+      !Array.from(noteMetadata.values()).includes("test");
+
+    expect(noteIsRenamedInMetadata).to.equal(true);
   });
 });

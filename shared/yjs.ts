5b4d993782479add60228488400207345d971327
SeePeaJay <cpj99@proton.me>
2025-07-23 22:33:52 -0700

    use pre-encoded Yjs update as default note template

--- shared/yjs.ts
+++ shared/yjs.ts
@@ -1,23 +1,29 @@
+import * as base64js from "base64-js";
 import * as Y from "yjs";
 
 function getDefaultYdocUpdate() {
-  const yDoc = new Y.Doc();
-  const yXmlFragment = yDoc.getXmlFragment("default");
-  const frontmatter = new Y.XmlElement("frontmatter");
-  const paragraph = new Y.XmlElement("paragraph");
+  // const yDoc = new Y.Doc();
+  // const yXmlFragment = yDoc.getXmlFragment("default");
+  // const frontmatter = new Y.XmlElement("frontmatter");
+  // const paragraph = new Y.XmlElement("paragraph");
 
-  yXmlFragment.push([frontmatter, paragraph]);
+  // yXmlFragment.push([frontmatter, paragraph]);
 
-  return Y.encodeStateAsUpdate(yDoc);
+  // console.log(base64js.fromByteArray(Y.encodeStateAsUpdate(yDoc)));
+
+  const template =
+    "AQKRjPGpCQAHAQdkZWZhdWx0Awtmcm9udG1hdHRlcoeRjPGpCQADCXBhcmFncmFwaAA="; // instead of generating a new fragment every time (which could cause duplicate nodes), we use a fixed, pre-encoded Yjs update as our template
+
+  return base64js.toByteArray(template);
 }
 
 function getDefaultMetadataYdocArray() {
   const ydoc = new Y.Doc();
-  const ymap = ydoc.getMap('noteMetadata');
+  const ymap = ydoc.getMap("noteMetadata");
 
   ymap.set("starred", "Starred");
 
-  return Y.encodeStateAsUpdate(ydoc);
+  return Y.encodeStateAsUpdate(ydoc); // no need to use pre-encoded fragment since ymap is different
 }
 
 export { getDefaultYdocUpdate, getDefaultMetadataYdocArray };

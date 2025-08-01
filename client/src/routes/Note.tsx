07c2955637fa250867e9f989926e7ac95562dabc
SeePeaJay <cpj99@proton.me>
2025-08-01 15:38:54 -0700

    make Note component check for starred before rendering editor
    * This fixes the ydoc index error that occurs when a user clears their cache, refreshes on /app/notes/starred, makes edits, and then logs in

--- client/src/routes/Note.tsx
+++ client/src/routes/Note.tsx
@@ -1,14 +1,23 @@
 import { useParams } from "react-router-dom";
 import Editor from "../components/Editor";
+import InitialLoadingScreen from "../components/InitialLoadingScreen";
+import { useStatelessMessenger } from "../contexts/StatelessMessengerContext";
 
 function Note() {
   const { noteId: noteIdParam } = useParams();
+  const { starredId } = useStatelessMessenger();
 
   return (
-    <div className="mx-auto w-[90vw] p-8 lg:w-[50vw]">
-      <Editor key={noteIdParam!} noteId={noteIdParam!} />{" "}
-      {/* Note route must have a param; key forces remount when param changes */}
-    </div>
+    <>
+      {starredId ? (
+        <div className="mx-auto w-[90vw] p-8 lg:w-[50vw]">
+          <Editor key={noteIdParam!} noteId={noteIdParam!} />{" "}
+          {/* Note route must have a param; key forces remount when param changes */}
+        </div>
+      ) : (
+        <InitialLoadingScreen />
+      )}
+    </>
   );
 }
 

fc14dad15f9547f5573f742bf79f3b5aa626902f
SeePeaJay <cpj99@proton.me>
2025-12-05 21:46:07 -0800

    setup github actions
    * adjust test file to make it work headlessly

--- client/src/components/TitleEditor.cy.tsx
+++ client/src/components/TitleEditor.cy.tsx
@@ -24,9 +24,10 @@ it("renames correctly", () => {
     </MemoryRouter>,
   );
 
-  cy.get("h1").click().type("ing{enter}");
+  cy.get("h1").as("title").click();
+  cy.get("@title").type("ing{enter}");
 
-  cy.get("h1").should("have.text", "testing");
+  cy.get("@title").should("have.text", "testing");
   cy.wait(1000); // wait for db update; below is not designed to rerun when assertion fails
   cy.then(() => db.notes.get({ title: "testing" })).should((note) => {
     expect(note?.title).to.contain("testing");
